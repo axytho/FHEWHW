97b8f92829fde93785882aeb76b2152003bdcc8e
axytho <jojojo.jonas888@hotmail.com>
2021-04-02 22:34:36 +0200

    Created Signed DigitDecompose

---
+++ new/signedDigitDecompose.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/02/2021 10:24:28 PM
+// Design Name: 
+// Module Name: signedDigitDecompose
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module signedDigitDecompose(
+    input clk,
+    input [`DATA_SIZE_ARB-1:0]  value_in
+    );
+    
+
+endmodule
